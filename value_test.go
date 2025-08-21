9183b8eb833ed2750c1d4a00103135a3239363ec
Viacheslav Poturaev <vearutop@gmail.com>
2025-08-21 12:57:02 +0200

    Infer var type from string

---
+++ value_test.go
@@ -0,0 +1,35 @@
+package vars_test
+
+import (
+	"testing"
+
+	"github.com/godogx/vars"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInfer(t *testing.T) {
+	for _, tc := range []struct {
+		s string
+		v any
+	}{
+		{"", ""},
+		{"123", int64(123)},
+		{"123.45", 123.45},
+		{"true", true},
+		{"false", false},
+		{"null", nil},
+		{`"bla\nbla"`, "bla\nbla"},
+		{`"bla\nbl...`, "infer string value \"bla\\nbl...: unexpected end of JSON input"},
+		{`{"foo":"bar"}`, map[string]any{"foo": "bar"}},
+		{`["abc", 1, false, null]`, []any{"abc", 1.0, false, nil}},
+		{`{"foo":"ba....`, "infer JSON value {\"foo\":\"ba....: unexpected end of JSON input"},
+	} {
+		t.Run(tc.s, func(t *testing.T) {
+			v := vars.Infer(tc.s)
+			if err, ok := v.(error); ok {
+				v = err.Error()
+			}
+			assert.Equal(t, tc.v, v)
+		})
+	}
+}
